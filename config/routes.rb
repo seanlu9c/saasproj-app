749b7de31b61697b6c22f7dc49731bde9c317825
Sean <seanlu9c@gmail.com>
2017-01-24 07:56:18 +0000

    bugfix confirmations_controller.rb with s

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   # *MUST* come *BEFORE* devise's definitions (below)
   as :user do   
     # match '/user/confirmation' => 'milia/confirmations#update', :via => :put, :as => :update_user_confirmation
-    # match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
-    match 'user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
+    match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
+    # match 'user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
 
   end
 
