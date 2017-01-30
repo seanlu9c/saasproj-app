b29d9190a08549c9b2f63468172f7217e015aace
Sean <seanlu9c@gmail.com>
2017-01-30 12:37:06 +0000

    Create project user association

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,7 +4,9 @@ class Project < ActiveRecord::Base
   
   validates_uniqueness_of :title
   has_many :artifacts, dependent: :destroy   ##### if delete project, all actifact will be deleted
-  
+  has_many :user_projects
+  has_many :users, through: :user_projects
+
   
   
   validate :free_plan_can_only_have_one_project ## custom validation
@@ -20,16 +22,34 @@ class Project < ActiveRecord::Base
   
   ###   PLACEHOLDER codes ..
   ### temporary ... to delete later
-  def self.by_plan_and_tenant(tenant_id)
-    tenant = Tenant.find(tenant_id)
+  # def self.by_plan_and_tenant(tenant_id)
+  #   tenant = Tenant.find(tenant_id)
     
+  #   if tenant.plan == 'premium'
+  #     tenant.projects    ## grab all projects that tenant has
+  #   else
+  #     tenant.projects.order(:id).limit(1)
+  #   end
+  # end
+  
+  def self.by_user_plan_and_tenant(tenant_id, user)
+    tenant = Tenant.find(tenant_id)
     if tenant.plan == 'premium'
-      tenant.projects    ## grab all projects that tenant has
+      if user.is_admin?
+        tenant.projects
+      else
+        user.projects.where(tenant_id: tenant.id)
+      end
     else
-      tenant.projects.order(:id).limit(1)
+      if user.is_admin?
+        tenant.projects.order(:id).limit(1)
+      else
+        user.projects.where(tenant_id: tenant.id).order(:id).limit(1) ## order by :id
+      end
     end
   end
   
+
 
   
 
